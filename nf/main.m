1c8b31214e76d1688ef9714d2d45451b9534dfc1
Thaddeus O. Cooper <tcooper@nomoreboxes.com>
2022-05-23 11:29:54 -0700

    Initial Commit

---
+++ nf/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  nf
+//
+//  Created by Thaddeus Cooper on 5/23/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
