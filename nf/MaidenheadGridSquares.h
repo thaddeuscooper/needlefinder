c368043c99bee621a921081a2fdbc677e8227d0c
Thaddeus O. Cooper <tcooper@nomoreboxes.com>
2023-02-09 19:10:11 -0800

    Added structure for each state's grid squares.

---
+++ nf/MaidenheadGridSquares.h
@@ -0,0 +1,18 @@
+//
+//  MaidenheadGridSquares.h
+//  nf
+//
+//  Created by Thaddeus Cooper on 2/9/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MaidenheadGridSquares : NSObject
+
+@property NSArray *maidenheadGridSquares;
+
+@end
+
+NS_ASSUME_NONNULL_END
